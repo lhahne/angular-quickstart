762472462b189a0f632ff2578fc6e866ee735587
Lauri Hahne <lauri.hahne@gmail.com>
2015-06-14 20:57:18 +0300

    complete example

--- src/todoModule.js
+++ src/todoModule.js
@@ -1,5 +1,7 @@
 'use strict'
 
+require('./todo.css')
+
 angular
   .module('todo', [])
   .directive('todos', () => ({
@@ -10,6 +12,9 @@ angular
 
       $scope.todos = [{name: 'Pese pyykit', completed: false}, {name: 'Vie roskat', completed: true}]
 
+      $scope.createTodo = (name) => {
+        $scope.todos.push({name, completed: false})
+      }
     }]
   }))
 

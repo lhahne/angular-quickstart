eed7187253cfc26026c768b625368a6a16aeba35
Lauri Hahne <lauri.hahne@gmail.com>
2015-06-16 13:23:29 +0300

    solutions

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
-'use strict'
+'use strict';
 
-angular = require('angular')
+angular = require('angular');
 
-angular.module('quickstart', [])
+angular
+  .module('quickstart', [])
+  .controller('TodoListController', ['$scope', '$http', function ($scope, $http) {
+    $scope.items = [{name: 'imuroi', finished: false},
+      {name: 'tiskaa', finished: false}];
+
+    $scope.addItem = function(newItem) {
+      $scope.items.push({name: newItem, finished: false});
+    };
+
+    $scope.removeItem = function(index) {
+      $scope.items.splice(index, 1);
+    };
+  }]);
